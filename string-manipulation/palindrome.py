0d431e95393a3783f7704e8610ba9cd2afd42fb9
kzuudev <kzuudev@gmail.com>
2025-11-30 08:36:11 +0800

    feat: check if the word is palindrome

---
+++ string-manipulation/palindrome.py
@@ -0,0 +1,18 @@
+
+
+def palindrome(string):
+
+
+    for i in range(len(string)):
+        for j in range(len(string)):
+            if string[i] == string[j - 1]:
+                print(f"The {string} is a palindrome")
+                return True
+            else:
+                print(f"The {string} is not palindrome")
+                return False
+
+
+
+word = "noon"
+print(palindrome(word))
