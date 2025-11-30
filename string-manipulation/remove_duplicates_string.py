1bb0445cbe0fa2b954673b56784e752a77a3a7b2
kzuudev <kzuudev@gmail.com>
2025-11-30 09:16:17 +0800

    refactor: add comment function skip duplicates and do nothing

--- string-manipulation/remove_duplicates_string.py
+++ string-manipulation/remove_duplicates_string.py
@@ -7,11 +7,12 @@ def remove_duplicates(string):
     for i in range(len(string)):
        if string[i] not in new_string:
            new_string += string[i]
+        # else do nothing and just skip duplicates
 
     return new_string
 
 
-word = "banana"
+word = "kevinjohnburat"
 
 
 print(remove_duplicates(word))
